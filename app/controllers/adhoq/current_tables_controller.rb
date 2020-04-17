892120fe6cbce9d3c6b42b8c00ed90e46231b1b6
Roman Parashchenko <romikoops1@gmail.com>
2020-04-17 11:03:03 +0200

    try to see how tables look

--- app/controllers/adhoq/current_tables_controller.rb
+++ app/controllers/adhoq/current_tables_controller.rb
@@ -4,11 +4,26 @@ module Adhoq
 
     def index
       hidden_model_names = Array(Adhoq.config.hidden_model_names)
-      hidden_model_names << 'ActiveRecord::SchemaMigration'
-      hidden_model_names << 'ApplicationRecord'
+      hidden_model_names << 'SchemaMigration' # service
+      hidden_model_names << 'ApplicationRecord' # service
+      hidden_model_names << 'ArInternalMetadatum'
+      hidden_model_names << 'AdhoqQuery'
+      hidden_model_names << 'AdhoqExecution'
+      hidden_model_names << 'AdhoqReport'
+      hidden_model_names << 'PspPayment' # old table?
+      hidden_model_names << 'CountriesPaymentMethod' # hbtm
+      hidden_model_names << 'SutorMessage'
+      hidden_model_names << 'SutorReconciliation'
+      hidden_model_names << 'PartnerOrdersSutorReconciliation'
+      hidden_model_names << 'BankTransactionsOrder' 
+      hidden_model_names << 'Version' # papertrail
+      hidden_model_names << 'DiscountsOrder' # hbtm
+      hidden_model_names << 'ActiveStorageBlob'
+      hidden_model_names << 'ActiveStorageAttachment'
 
-      @ar_classes = ActiveRecord::Base.descendants.
-        reject {|klass| klass.abstract_class? || hidden_model_names.include?(klass.name) || klass.name.nil? }.
+      @ar_classes = ActiveRecord::Base.connection.tables.map { |model| model.capitalize.singularize.camelize }.
+        reject { |model_name| hidden_model_names.include?(model_name) }.
+        map { |model_name| model_name.constantize }.
         sort_by(&:name)
 
       render layout: false
